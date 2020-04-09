1ede8d337420d1128abd7f70e8730864ecce1362
alnasimul <alnasimul@gmail.com>
2020-04-09 23:16:48 +0600

    difference between double and triple equals

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,12 @@
+// double equals mean check by value where value type doesn't matter 
+// triple equals mean check by value and type where value and type is strictly recommended
+
+let num1 = 1;
+let num2 = true;
+
+if(num1 === num2){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
