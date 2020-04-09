20bc45e25171c6bdbe278f927e628e2022235429
alnasimul <alnasimul@gmail.com>
2020-04-09 22:36:14 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// Falsy
+// false
+// 0
+// ""
+// null
+// undefined
+// NaN
+// Truthy
+// " "
+// "0"
+// {}
+// []
+
+let name = "Sadaf";
+
+if(name){
+    console.log("Condition is true");
+}else{
+    console.log("Condition is false");
+}
