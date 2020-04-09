d9bb6592fbbda0904c24ad7f5eea6c15c088f42b
alnasimul <alnasimul@gmail.com>
2020-04-09 23:07:17 +0600

    null vs undefined cleared for interview

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,24 +1,28 @@
-let pakhi;
+// let pakhi;
 
-function add(num1,num2){
-    console.log(num1 + num2);
+// function add(num1,num2){
+//     console.log(num1 + num2);
 
-    return
-}
+//     return
+// }
 
 
-function add(num1, num2){
-    console.log(num1, num2);
-}
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
 
-let result = add(2,20);
+// let result = add(2,20);
 
-console.log(result);
+// console.log(result);
 
-const premik = {name:"smart dude", phone:458421};
+// const premik = {name:"smart dude", phone:458421};
 
-let fun = undefined;
+// let fun = undefined;
 
-console.log(fun);
+// console.log(fun);
 
-console.log(premik.gf);
+// console.log(premik.gf);
+
+let ages = [2, 5, 9]
+
+console.log(ages[11]);
