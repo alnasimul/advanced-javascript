40cb61eac3c872c150366c1085c909fa01e51353
alnasimul <alnasimul@gmail.com>
2020-04-09 22:56:48 +0600

    null vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+let pakhi;
+
+function add(num1,num2){
+    console.log(num1 + num2);
+
+    return
+}
+
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+let result = add(2,20);
+
+console.log(result);
+
+const premik = {name:"smart dude", phone:458421};
+
+let fun = undefined;
+
+console.log(fun);
+
+console.log(premik.gf);
