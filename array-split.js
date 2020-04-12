73a14bb4b35ef7e358d92d9d20b5a0e0d55b85fa
alnasimul <alnasimul@gmail.com>
2020-04-12 22:33:48 +0600

    learned about basic array split function on js

---
+++ array-split.js
@@ -0,0 +1,11 @@
+let nums = [1,2,3,4,5,6,7,8,9];
+
+//let update = nums.slice(1,4);
+
+//let removed = nums.splice(1,2,10,11);
+
+//console.log(removed);
+
+let join = nums.join("-");
+console.log(join);
+console.log(nums);
